db3698d529ed90470153cd3019392dddc8075476
Kasun Dulara <kasundularaam@gmail.com>
2022-11-15 19:52:23 +0530

    code cleaned

--- lib/core/configs/configs.dart
+++ lib/core/configs/configs.dart
@@ -1,9 +1,1 @@
 const String staticUrlsJson = "http://critssl.com/ip/garbage.json";
-
-const String server = "http://18.234.204.199";
-const String httpPort = "8000";
-const String httpUrl = "$server:$httpPort/api";
-
-const String socketServer = "http://192.168.8.101";
-const String socketPort = "3000";
-const String socketUrl = "$socketServer:$socketPort";
